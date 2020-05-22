22b56d3c31542227fbe43b5d3ec5f16803b27e9b
Andres Perez Campillo <tomailsandstuff@gmail.com>
2020-05-22 16:39:15 -0500

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    [Range(10F, 60F)]
+    private float moveSpeed = 10F;
+
+    private float vVal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vVal = Input.GetAxis("Vertical");
+        transform.Translate(transform.forward * vVal * moveSpeed * Time.deltaTime);
+    }
+}
