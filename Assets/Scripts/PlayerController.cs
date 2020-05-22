49cbc625690de02dadbca7c66d4a974f55561180
Andres Perez Campillo <tomailsandstuff@gmail.com>
2020-05-22 16:52:21 -0500

    Added jump, rotation to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     [Range(10F, 60F)]
     private float moveSpeed = 10F;
 
+    [SerializeField]
+    [Range(30F, 60F)]
+    private float rotationSpeed = 30F;
+
+    [SerializeField]
+    [Range(1F, 20F)]
+    private float jumpForce = 1F;
+
     private float vVal;
+    private float hVal;
+
+    private Rigidbody myRigidbody;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         vVal = Input.GetAxis("Vertical");
         transform.Translate(transform.forward * vVal * moveSpeed * Time.deltaTime);
+
+        hVal = Input.GetAxis("Mouse X");
+        transform.Rotate(transform.up * rotationSpeed * hVal * Time.deltaTime);
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
